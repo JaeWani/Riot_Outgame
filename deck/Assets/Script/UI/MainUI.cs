6a09b7f95947574621794b2960289a5ff324343d
Jaewani <sdh220216@sdh.hs.kr>
2023-05-22 19:39:28 +0900

    카드 추가

---
+++ deck/Assets/Script/UI/MainUI.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUI : MonoBehaviour
+{
+    [Header ("Screen")]
+    [SerializeField] RectTransform ScreenRect;
+    [SerializeField] Vector3 DeckScreenPos;
+    [SerializeField] Vector3 MainScreenPos;
+    [SerializeField] Vector3 ShopScreenPos;
+    [SerializeField] Vector3 CurrentScreen;
+
+    [Header ("Button")]
+    [SerializeField] Button MainScreenBtn;
+    [SerializeField] Button DeckScreenBtn;
+    [SerializeField] Button ShopScreenBtn;
+
+    private void Awake()
+    {
+        MainScreenBtn.onClick.AddListener(() => SetScreen(MainScreenPos));
+        DeckScreenBtn.onClick.AddListener(() => SetScreen(DeckScreenPos));
+        ShopScreenBtn.onClick.AddListener(() => SetScreen(ShopScreenPos));
+    }
+    private void Start() 
+    {
+        CurrentScreen = MainScreenPos;
+    }
+    private void Update() 
+    {
+        ScreenMotion(5000);
+    }
+    void ScreenMotion(float speed)
+    {
+        ScreenRect.anchoredPosition = Vector2.MoveTowards(ScreenRect.anchoredPosition,CurrentScreen,speed * Time.deltaTime);
+    }
+    void SetScreen(Vector3 ScreenPos)
+    {
+        CurrentScreen = ScreenPos;
+    }
+}
